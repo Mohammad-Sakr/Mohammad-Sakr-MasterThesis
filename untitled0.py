4cf877741d1a8fc30006eb4cd0e0f13b09d8db70
Mohammad-Sakr <86956162+Mohammad-Sakr@users.noreply.github.com>
2022-04-18 02:13:50 +0200

    Create untitled0.py

---
+++ untitled0.py
@@ -0,0 +1,23 @@
+
+import numpy as np
+import matplotlib.pyplot as plt
+import scipy as scipy
+ 
+a=30
+N=1000
+x=np.linspace(-a,a,N)
+y=np.array([1 if np.abs(n)<=1 else 0 for n in x])
+
+Tstep=2*a/N
+X=np.linspace(-1/(Tstep*2),1/(Tstep*2),N)
+Y=scipy.fft.fftshift(scipy.fft(y))
+
+
+plt.figure()
+plt.plot(x,y)
+plt.show()
+
+plt.figure()
+plt.plot(X,np.abs(Y))
+plt.show()
+
